ebb51130c12df534ebff02ea92b1cd63b192427d
vyshnav suresh <93926562+VyshnavSuresh@users.noreply.github.com>
2023-08-05 13:20:34 +0530

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
-'use client'
 import Movingbanner from '@/components/Movingbanner'
 import React from 'react'
-import HeroText from '@/components/main/Section1'
 import Teams from '@/components/main/Teams'
 import Achivements from '@/components/main/Achivements'
+import HeroText from '@/components/main/Section1'
 
 const page = () => {
   return (
