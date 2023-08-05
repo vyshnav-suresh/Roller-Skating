3ade5b5289473ac02c5f66f7bdacd9d034356c4a
vyshnav suresh <93926562+VyshnavSuresh@users.noreply.github.com>
2023-08-05 19:18:18 +0530

    Testing path

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
+import Movingbanner from '@/components/Movingbanner'
 import React from 'react'
 
 
 const page = () => {
   return (
     <div className='bg-white_cream text-white_cream'>
-    {/* <Movingbanner/>
-  <HeroText/>
+    <Movingbanner/>
+  {/* <HeroText/>
   <Teams/>
   <Achivements/> */}
   fsdfsd
