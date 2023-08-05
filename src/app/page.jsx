7256a97423a073b4187849375523f65fae3e5ca4
vyshnav suresh <93926562+VyshnavSuresh@users.noreply.github.com>
2023-08-05 18:22:33 +0530

    Up

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 'use client'
-import Movingbanner from '../components/Movingbanner'
+import Movingbanner from '@/components/Movingbanner'
 import React from 'react'
-import Teams from '../components/Teams'
-import HeroText from '../components/Section1'
-import Achivements from '../components/Achivements'
+import Teams from '@/components/main/Teams'
+import Achivements from '@/components/main/Achivements'
+import HeroText from '@/components/main/Section1'
 
 const page = () => {
   return (
